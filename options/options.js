66637d858f1707a1d59935c14212a56366d8bf3c
django <django@49b1c411d110>
2024-06-23 14:25:20 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dry-butterfly-1",
+  url: "https://dry-butterfly-1.botics.co",
+  api: "https://dry-butterfly-1.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
